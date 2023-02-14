1629c7df9696964a000f08b76fc29bc23808fb3c
hostomani <hostomani@gmail.com>
2023-02-14 13:54:14 +0000

    add hl_base security group administrator

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'HL',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -29,6 +29,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/partner_application.xml',
         'views/application_service.xml',
         'views/application_service_document.xml',
